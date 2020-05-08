184b9be52f31e24a935d22f9142427415ac358d6
Alessandro Lion <alessandro.lion@outlook.com>
2020-05-08 16:00:11 +0200

    Add project files.

---
+++ FilesName2CSV/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace FilesName2CSV
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonBrowse = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.buttonStart = new System.Windows.Forms.Button();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxAppend = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Location = new System.Drawing.Point(6, 19);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(384, 20);
+            this.textBoxFolder.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonBrowse);
+            this.groupBox1.Controls.Add(this.textBoxFolder);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(460, 56);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Folder Selection";
+            // 
+            // buttonBrowse
+            // 
+            this.buttonBrowse.Location = new System.Drawing.Point(396, 16);
+            this.buttonBrowse.Name = "buttonBrowse";
+            this.buttonBrowse.Size = new System.Drawing.Size(45, 23);
+            this.buttonBrowse.TabIndex = 1;
+            this.buttonBrowse.Text = "...";
+            this.buttonBrowse.UseVisualStyleBackColor = true;
+            this.buttonBrowse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 241);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(460, 319);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBoxAppend);
+            this.groupBox2.Controls.Add(this.radioButton2);
+            this.groupBox2.Controls.Add(this.radioButton1);
+            this.groupBox2.Controls.Add(this.buttonStart);
+            this.groupBox2.Location = new System.Drawing.Point(12, 75);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(460, 136);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Parsing Options";
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Checked = true;
+            this.radioButton2.Location = new System.Drawing.Point(7, 19);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(128, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "None (NameList Only)";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(188, 19);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(266, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Underscore sep values ending with E cent  amount";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // buttonStart
+            // 
+            this.buttonStart.Location = new System.Drawing.Point(132, 107);
+            this.buttonStart.Name = "buttonStart";
+            this.buttonStart.Size = new System.Drawing.Size(183, 23);
+            this.buttonStart.TabIndex = 0;
+            this.buttonStart.Text = "Start";
+            this.buttonStart.UseVisualStyleBackColor = true;
+            this.buttonStart.Click += new System.EventHandler(this.buttonStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ForeColor = System.Drawing.SystemColors.GrayText;
+            this.label1.Location = new System.Drawing.Point(12, 214);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Ready";
+            // 
+            // checkBoxAppend
+            // 
+            this.checkBoxAppend.AutoSize = true;
+            this.checkBoxAppend.Location = new System.Drawing.Point(7, 83);
+            this.checkBoxAppend.Name = "checkBoxAppend";
+            this.checkBoxAppend.Size = new System.Drawing.Size(141, 17);
+            this.checkBoxAppend.TabIndex = 3;
+            this.checkBoxAppend.Text = "Append on multiple Start";
+            this.checkBoxAppend.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 585);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "FilesName2CSV by Alex Pi Lion";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button buttonBrowse;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.Button buttonStart;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxAppend;
+    }
+}
+
---
+++ FilesName2CSV/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FilesName2CSV
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static bool ParseFileName(string sInputName, string sMode, out string sParsedRow)
+        {
+            string commasep = ",";
+            string valwrap="\"";
+            string decimalsep = ".";
+            try
+            {
+                sParsedRow = valwrap+sInputName+valwrap;
+                switch (sMode)
+                {
+                    case "R":
+                        //sParsedRow
+                        String mypattern = @"^([0-9]{8})[-]{0,1}([0-9]{0,8})_(\w{1,50})_([0-9]{1,12})E([0-9]{2})cent.pdf";
+                        foreach (System.Text.RegularExpressions.Match m in
+                        System.Text.RegularExpressions.Regex.Matches(sInputName, mypattern))
+                        {
+                            //Groups 1-3
+                            for (int i = 1; i < 4; i++)
+                            {
+                                sParsedRow += commasep + valwrap + m.Groups[i].Value + valwrap;
+                            }
+                            //Groups 4,5 amount
+                            sParsedRow += commasep + valwrap + m.Groups[4].Value +  decimalsep + m.Groups[5].Value + valwrap;
+                        }
+                            break;
+                    default: // N and unmanaged cases
+                        break;
+                }
+                return true;
+            }
+            catch (Exception er)
+            {
+                sParsedRow = "Unexpected Error" + er.Message;
+                return false;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            buttonStart.Enabled = false;
+            textBoxFolder.Text = "";
+            if (folderBrowserDialog1.ShowDialog() == DialogResult.OK)
+            {
+                textBoxFolder.Text = folderBrowserDialog1.SelectedPath;
+                buttonStart.Enabled = true;
+            }
+            else
+            {
+                label1.Text = "Folder Selection Cancelled, please repeat.";
+            };
+        }
+
+        private void buttonStart_Click(object sender, EventArgs e)
+        {
+            if (textBoxFolder.Text!="")
+            {
+                if (!checkBoxAppend.Checked)
+                    {
+                    richTextBox1.ResetText(); //CLEANUP
+                    }
+                label1.Text = "Reading files from " + textBoxFolder.Text;
+
+                try
+                {
+                    string sourceDirectory = textBoxFolder.Text;
+                    string parsedVal="";
+                    var txtFiles = Directory.EnumerateFiles(sourceDirectory, "*.*", SearchOption.AllDirectories);
+                    string sParsingMode = "N"; //Default N = Only names
+                    if (radioButton1.Checked)
+                        {
+                          sParsingMode = "R"; //R = Refound style documents
+                        }
+                    else
+                        {
+                          //TO DO: Manage further options
+                        }
+
+                    foreach (string currentFile in txtFiles)
+                    {
+                        string fileName = currentFile.Substring(sourceDirectory.Length + 1);
+                        label1.Text = "Found " + fileName;
+                        if (ParseFileName(fileName, sParsingMode, out parsedVal))
+                            {
+                            richTextBox1.AppendText(parsedVal);
+                            richTextBox1.AppendText(Environment.NewLine);
+                            }
+                    }
+                    System.Windows.Forms.Clipboard.SetText(richTextBox1.Text);
+                }
+                catch (Exception er)
+                {
+                    label1.Text="ERROR " + (er.Message);
+                }
+            }
+        }
+    }
+}
